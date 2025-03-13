14a1a4c11e63729d83e80b1ac5d2259a67441a8b
SkipPP <pierre.peraldi31@gmail.com>
2025-03-13 01:43:55 +0100

    maybe a fix for headers ? if not, try with the auth client

--- app/routes/dashboard/route.tsx
+++ app/routes/dashboard/route.tsx
@@ -14,10 +14,9 @@ import {
 export const Route = createFileRoute("/dashboard")({
   component: DashboardLayout,
   beforeLoad: async ({ context }) => {
-    if (!context.user) {
+    /* if (!context.user) {
       throw redirect({ to: "/signin" });
-    }
-
+    } */
     // `context.queryClient` is also available in our loaders
     // https://tanstack.com/start/latest/docs/framework/react/examples/start-basic-react-query
     // https://tanstack.com/router/latest/docs/framework/react/guide/external-data-loading
--- app/routes/dashboard/users/index.tsx
+++ app/routes/dashboard/users/index.tsx
@@ -1,11 +1,27 @@
 import { z } from "zod";
+import { queryOptions } from "@tanstack/react-query";
+import { createServerFn } from "@tanstack/react-start";
 import { createFileRoute } from "@tanstack/react-router";
+import { getWebRequest } from "@tanstack/react-start/server";
 
+import { auth } from "~/lib/server/auth";
 import authClient from "~/lib/utils/auth-client";
 
 import { DataTable } from "~/lib/components/DataTable";
 
 import { columns } from "../../../../lib/components/userColumns";
+
+const listUsers = createServerFn({ method: "GET" }).handler(async () => {
+  const { headers } = getWebRequest()!;
+  const users = await auth.api.listUsers({ headers, query: { limit: 10 } });
+
+  return { users: users.users, total: users.total };
+});
+
+const listUsersQuery = queryOptions({
+  queryKey: ["users"],
+  queryFn: ({ signal }) => listUsers({ signal }),
+});
 
 export const Route = createFileRoute("/dashboard/users/")({
   component: RouteComponent,
@@ -16,24 +32,14 @@ export const Route = createFileRoute("/dashboard/users/")({
     limit: search.limit,
   }),
   loader: async ({ context, deps }) => {
-    const users = await authClient.admin.listUsers({
-      query: {
-        limit: deps.limit,
-      },
-    });
+    const users = await context.queryClient.fetchQuery(listUsersQuery);
 
-    if (users.error) {
-      console.error(users.error);
-
-      throw new Error(`${users.error.status} ${users.error.statusText}`);
-    }
-
-    return { users: users.data };
+    return users;
   },
 });
 
 function RouteComponent() {
   const { users } = Route.useLoaderData();
 
-  return <DataTable data={users.users} columns={columns} />;
+  return <DataTable data={users} columns={columns} />;
 }
